7ca952c719c7d45e9db5bfb41c8cc96528e6036a
quzg <1400863951@qq.com>
2016-03-26 14:58:31 +0800

    iOS-Project
    A template for Project

---
+++ iOS-Project/FolkMusic/FolkMusic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FolkMusic
+//
+//  Created by quzg on 16/3/26.
+//  Copyright © 2016年 quzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
