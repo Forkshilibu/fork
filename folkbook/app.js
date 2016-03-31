7c770bc72bfcf02a13cef0b6a6d1b8d28869ba3b
joeycz <muxiesan1989@gmail.com>
2016-03-31 15:17:10 +0800

    基础

--- folkbook/app.js
+++ folkbook/app.js
@@ -4,7 +4,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var todos = require('./routes/todos');
+var routes = require('./routes');
 var cloud = require('./cloud');
 var jsmediatags = require('jsmediatags');
 var React = require('react');
@@ -14,8 +16,8 @@ var app = express();
 
 // 设置 view 引擎
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jsx');
-//app.set('view engine', 'ejs');
+// app.set('view engine', 'jsx');
+app.set('view engine', 'ejs');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static('public'));
 
@@ -51,6 +53,9 @@ app.use(function(req, res, next) {
   d.run(next);
 });
 
+
+
+
 app.get('/', function(req, res) {
   res.render('index', { currentTime: new Date() });
 });
@@ -72,16 +77,58 @@ app.get('/getID3',function(req, res, next){
   });
 });
 
-app.get('/reacttest', require('./routes').index);
+var AV = require('leanengine');
+var recommendList = AV.Object.extend('recommendList');
+// app.post('/musiclist',function(req, res, next){
+//   console.log(req.body.count);
+//   var query = new AV.Query(recommendList);
+//   query.descending('createdAt');
+//   query.skip(req.body.page*req.body.count);
+//   query.limit(req.body.count);
+//   query.find({
+//     success: function(results) {
+//       res.send({code: 0,results: results});
+//     },
+//     error: function(err) {
+//       next(err);
+//     }
+//   });
+// })
+
+var musiclist = require('./routes/musiclist');
+app.use('/musiclist',musiclist);
+
+// 用户注册
+// app.use(AV.Cloud.CookieSession({ secret: 'my secret', maxAge: 3600000, fetchUser: true }));
+// var user = new AV.User();
+// user.set('username', 'hjiang');
+// user.set('password', 'f32@ds*@&dsa');
+// user.set('email', 'hang@leancloud.rocks');
+
+// other fields can be set just like with AV.Object
+// user.set('phone', '186-1234-0000');
+// user.signUp().then(function(user) {
+//   // 注册成功，可以使用了
+//   console.log(user);
+// }, function(error) {
+//   // 失败了
+//   console.log('Error: ' + error.code + ' ' + error.message);
+// });
+
+app.get('/reacttest', routes.index);
 
 
 // 可以将一类的路由单独保存在一个文件中
 app.use('/todos', todos);
+// app.use('/reacttest', routes.index);
 
 
 // 如果任何路由都没匹配到，则认为 404
 // 生成一个异常让后面的 err handler 捕获
-app.use(function(req, res, next) {
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(function(err, req, res, next) {
+  console.log(res);
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
--- folkbook/routes/index.js
+++ folkbook/routes/index.js
@@ -1,4 +1,5 @@
 
+
 exports.index = function(req, res){
   res.render('reactview', { name: '程郑',title: '远方' });
 };
